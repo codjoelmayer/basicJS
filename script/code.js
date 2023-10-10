a826a897d817d4e3242f7a82221e84dff5f2398b
codjoelmayer <tmjoelm4@gmail.com>
2023-10-10 12:58:24 +0200

    Updates

--- script/code.js
+++ script/code.js
@@ -1,1 +1,97 @@
-console.log("My name is Joel")
+// let firstName = 'Joel'
+// console.log("My name is "+ firstName)
+// console.log(firstName +" have 7 sisters")
+
+// console.log(`My name is ${firstName}`)
+// console.log(`${firstName} have 7 sisters`)
+
+// Single line
+
+/*
+Long description
+Or Multiple line of comment
+*/
+
+// Different ways of creating a variable
+/*
+1. var or you can make use of a variable name
+var name1 = 'Peter'
+name2 = 'John'
+2. let
+let salary1 = 50000
+3. const
+const country; 
+*/
+// let, or const
+
+// Data type
+/*
+- Primitive data type: string, number,
+bool or boolean, undefined, null
+- Dynamic data type: array, object
+*/
+// let isMale = true 
+// let subjects = ['HTML 5', 'CSS 3', 'Javascript']
+// console.log(subjects);
+// let person = {
+//     name: 'Joel',
+//     surname: 'Mukanya',
+//     age: 40
+// }
+// console.log(person)
+
+// let data = true
+// console.log(data);
+// console.log(typeof data);
+// let numbers = [1, 3, 5]
+// console.log(numbers[2]);
+// console.table(numbers)
+// console.warn("You are a bad person")
+// console.log(data[0]);
+// Operators
+/*
+- Arithmetic: +, -, /, *, ** (Exponent), % (Modulus)
+- Assignment: =, +=, -= etc
+- Comparison: >, ==, ===, >=, <, <= !=
+- Logical: ||, &&, !
+*/
+/*
+1. Create a remote repository and clone
+2. Change your folder into the cloned repo
+3. Create the following items
+    3.1: index.html
+    3.2: script
+        3.2.1: code.js
+4. Allow the user to enter the hours worked and rate. 
+Store those values inside of a variables such as 
+hours and rate
+5. Display the salary to a console using the below output
+Your salary is R50,000
+
+*/
+
+// console.log(
+//     `Sum of ${numb1} and ${numb2} is ${numb1 + numb2}`)
+// console.log(
+//     `Difference of ${numb2} and ${numb1} is ${numb2 - numb1}`)
+// console.log(
+//     `Product of ${numb1} and ${numb2} is ${numb1 * numb2}`)
+// console.log(`${numb2}/${numb1} = ${numb2 / numb1}`);
+// console.log(`${numb2}**2 ${numb2 ** 2}`);
+// console.log(`What is the reminder of 12/2 = ${12 % 2}`);
+
+// let numb1 = "3"
+// let numb2 = 3
+
+// let addition = numb1 + numb2
+// console.log(`Addition -> arithmetic: ${addition}`);
+
+// numb1 += numb2
+// console.log(`The value inside numb1 -> assignment op: ${numb1}`);
+
+// Comparison
+// console.log(numb1 === numb2);
+/*
+====
+
+*/
